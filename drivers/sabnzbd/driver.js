6b236d8edcff75e0267728e89f245a0a8d4a3925
swttt <Bas@Bass-iMac.fritz.box>
2016-06-28 01:30:55 +0200

    Fixed settings
    Settings survive reboot and are saved properly now.

--- drivers/sabnzbd/driver.js
+++ drivers/sabnzbd/driver.js
@@ -5,16 +5,10 @@ var request = require('request');
 // paired and active devices in your driver's memory.
 var devices = {};
 var intervalId = {};
-var currentPolling = 10;
 
 // the `init` method is called when your driver is loaded for the first time
 module.exports.init = function( devices_data, callback ) {
-    devices_data.forEach(function(device_data){
-        initDevice( device_data );
-
-        Homey.log(device_data.id);
-
-  })
+    devices_data.forEach(initDevice);
   //setInterval(monitor, 15000);
 
 
@@ -29,13 +23,21 @@ module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj,
     if(newSettingsObj.pollingrate < 5){callback( __('pair.settingschanged.not_under_5'), null );}
     clearInterval(intervalId[device_data.id]);
     delete intervalId[device_data.id];
-    initDevice( device_data, newSettingsObj );
+
+     devices[device_data.id].settings.host=newSettingsObj.host;
+     devices[device_data.id].settings.tcpport=newSettingsObj.tcpport;
+     devices[device_data.id].settings.apikey=newSettingsObj.apikey;
+     devices[device_data.id].settings.urlprefix=newSettingsObj.urlprefix;
+     devices[device_data.id].settings.pollingrate=newSettingsObj.pollingrate;
+
+    initDeviceInterval(device_data,devices[device_data.id].settings.pollingrate);
+  
 
     // always fire the callback, or the settings won't change!
     // if the settings must not be saved for whatever reason:
     // callback( "Your error message", null );
     // else
-    callback( "Settings saved!", true );
+    callback( null, true );
 
 }
 
@@ -43,6 +45,7 @@ module.exports.settings = function( device_data, newSettingsObj, oldSettingsObj,
 // the `added` method is called is when pairing is done and a device has been added
 module.exports.added = function( device_data, callback ) {
     initDevice( device_data );
+
     Homey.manager( 'insights' ).createLog('sab-' + device_data.id, {
         label: {
             nl: 'Download snelheid',
@@ -127,47 +130,62 @@ function getDeviceByData( device_data ) {
 }
 
 // a helper method to add a device to the devices list
-function initDevice( device_data, newSettingsObj ) {
+function initDevice( device_data, newSettingsObj, callback) {
+
+  module.exports.getSettings( device_data, function( err, settings ){
 
 
+    //migration from old v0.9.8 app with no settings in device
+    if (settings.pollingrate==undefined) {
+      Homey.log("No pollingrate found!");
+      settings = {
+        host: settings.host,
+        tcpport: settings.tcpport,
+        apikey: settings.apikey,
+        urlprefix: settings.urlprefix,
+        pollingrate: 10 };
+        devices[ device_data.id ].settings = settings;
+        initDeviceInterval(device_data,settings.pollingrate);
+        module.exports.setSettings( device_data, settings, function( err, settings ){
+            // ... dunno what to do here, think nothing...
+        })
+    }
+    else{
+      devices[ device_data.id ].settings = settings;
+      initDeviceInterval(device_data,settings.pollingrate);
+    }
+  })
     devices[ device_data.id ] = {};
     devices[ device_data.id ].data = device_data;
     devices[ device_data.id ].currentSlots = "not set";
-    if(newSettingsObj){
-      Homey.log("New settings found!");
-      devices[ device_data.id ].settings = newSettingsObj;
-    }
-    else{
-      Homey.log("Old settings used!");
-    module.exports.getSettings(device_data, function (err, settings) {
-    devices[ device_data.id ].settings = settings;
 
-  });
+
+
+
+
   }
 
-  if( typeof devices[ device_data.id ].settings !== 'undefined' ) {
-    if(typeof devices[ device_data.id ].settings.pollingrate !== 'undefined'){
-  //start polling device for readings every 10 seconds
 
-    currentPolling = devices[ device_data.id ].settings.pollingrate;
 
-    Homey.log("Polling settings found: " + currentPolling);
-    //currentPolling = devices[ device_data.id ].settings.pollingrate;
-  }
+
+
+
+function initDeviceInterval(device_data,pollingrate){
+  intervalId[device_data.id] = setInterval(function () {
+    monitorSab(devices[device_data.id], function(response){
+        //reserved for callback
+      })
+    }, pollingrate * 1000);
 }
-    intervalId[device_data.id] = setInterval(function () {
-      monitorSab(devices[device_data.id].data, function(response){
-          //reserved for callback
-        })
-      }, currentPolling * 1000);
 
-}
+
+
 function monitorSab(device_data, callback) {
 
 
-      var device = getDeviceByData(device_data);
+      var device = device_data;
 
-      Homey.log("Trying " + device.settings.host + " on port " + device.settings.tcpport + " and " + device.settings.urlprefix);
+      Homey.log("Trying " + device.settings.host + " on port " + device.settings.tcpport + " and " + device.settings.urlprefix + " Polling rate is: " + device.settings.pollingrate);
       if(device.settings.urlprefix == 'https'){var urlprefix = 'https://';}else{var urlprefix = 'http://';}
 
       var url = urlprefix + device.settings.host + ':' + device.settings.tcpport + '/api?mode=qstatus&output=json&apikey=' + device.settings.apikey;
